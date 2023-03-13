c8d09b2103dd5e45a17bf4755bc58b888f960c2c
tailancorreia <127754474+tailancorreia@users.noreply.github.com>
2023-03-13 12:46:45 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -14,4 +14,27 @@ OBJETIVO quando clicarmor no botão temos que mostrar a imagem de fundo correspo
 - passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado 
 */
 
-console.console.log(document);
+//- passo 1 - dar um jeito de pegar o elemento HTML dos botões
+const botoesCarrossel = document.querySelectorAll('.botao')
+const imagens = document.querySelectorAll('.imagem');
+
+//- passo 2 - dar um jeito de indetificar o clique do usuário no botão
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        console.log(indice);
+     //- passo 3 - desmarcar o botão selecionado anterior
+     const botaoSelecionado = document.querySelector('.selecionado');
+     botaoSelecionado.classList.remove('selecionado');
+
+     //- passo 4 - marcar o botão clicado como se estivesse selecionado
+     botao.classList.add('selecionado')
+     
+     //- passo 5 - esconder a imagem de funto anterior 
+     const imagemAtiva = document.querySelector('.ativa');
+     imagemAtiva.classList.remove('ativa');
+
+    //- passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado 
+    imagens[indice].classList.add('ativa')
+
+    })
+})

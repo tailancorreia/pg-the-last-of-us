b1238aa340453c7765f24ecf97e5737859de819f
tailancorreia <127754474+tailancorreia@users.noreply.github.com>
2023-03-13 11:27:05 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+/* 
+OBJETIVO quando clicarmor no botão temos que mostrar a imagem de fundo correspondente 
+
+- passo 1 - dar um jeito de pegar o elemento HTML dos botões
+
+- passo 2 - dar um jeito de indetificar o clique do usuário no botão
+
+- passo 3 - desmarcar o botão selecionado anterior
+
+- passo 4 - marcar o botão como se estivesse selecionado
+
+- passo 5 - esconder a imagem de funto anterior 
+
+- passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado 
+*/
+
+console.console.log(document);
